cea440f13132771220f30f6975e8ebf5c60fda3b
AndresOS1 <ordonezandres202@gmail.com>
2026-03-10 16:16:23 -0500

    refactor(navigation): modularize nav graph and route helpers

--- app/src/main/java/co/edu/uniautonoma/inclusivereadingar/presentation/navigation/AppDestinations.kt
+++ app/src/main/java/co/edu/uniautonoma/inclusivereadingar/presentation/navigation/AppDestinations.kt
@@ -1,5 +1,8 @@
 package co.edu.uniautonoma.inclusivereadingar.presentation.navigation
 
 object AppDestinations {
-    const val WEB_AR_ROUTE = "web_ar"
+    const val HOME_ROUTE = "home"
+    const val START_ROUTE = HOME_ROUTE
+    const val LOGIN_ROUTE = "login"
+    const val SCAN_CARD_ROUTE = "scan_card"
 }
